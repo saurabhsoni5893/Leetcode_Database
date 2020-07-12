71714aa1bad8a3293018930481a99acd9350ac1f
Saurabh Soni <saurabhsoni5893@gmail.com>
2020-07-12 01:39:51 -0500

    Create 12_586_Customer_Placing_the_Largest_Number_of_Orders.sql

---
+++ 12_586_Customer_Placing_the_Largest_Number_of_Orders.sql
@@ -0,0 +1,58 @@
+/*
+
+Query the customer_number from the orders table for the customer who has placed the largest number of orders.
+
+It is guaranteed that exactly one customer will have placed more orders than any other customer.
+
+The orders table is defined as follows:
+
+| Column            | Type      |
+|-------------------|-----------|
+| order_number (PK) | int       |
+| customer_number   | int       |
+| order_date        | date      |
+| required_date     | date      |
+| shipped_date      | date      |
+| status            | char(15)  |
+| comment           | char(200) |
+Sample Input
+
+| order_number | customer_number | order_date | required_date | shipped_date | status | comment |
+|--------------|-----------------|------------|---------------|--------------|--------|---------|
+| 1            | 1               | 2017-04-09 | 2017-04-13    | 2017-04-12   | Closed |         |
+| 2            | 2               | 2017-04-15 | 2017-04-20    | 2017-04-18   | Closed |         |
+| 3            | 3               | 2017-04-16 | 2017-04-25    | 2017-04-20   | Closed |         |
+| 4            | 3               | 2017-04-18 | 2017-04-28    | 2017-04-25   | Closed |         |
+Sample Output
+
+| customer_number |
+|-----------------|
+| 3               |
+
+*/
+
+-- SQL Schema:
+
+IF NOT EXISTS (SELECT * FROM SYSOBJECTS WHERE NAME = 'ORDERS' AND XTYPE = 'U')
+	CREATE TABLE ORDERS(
+	order_number int, 
+	customer_number int, 
+	order_date date, 
+	required_date date, 
+	shipped_date date, 
+	status char(15), 
+	comment char(200), 
+	PRIMARY key(order_number))
+
+Truncate table orders
+insert into orders (order_number, customer_number) values ('1', '1')
+insert into orders (order_number, customer_number) values ('2', '2')
+insert into orders (order_number, customer_number) values ('3', '3')
+insert into orders (order_number, customer_number) values ('4', '3')
+
+-- Solutions:
+
+SELECT TOP 1 CUSTOMER_NUMBER
+FROM ORDERS 
+GROUP BY CUSTOMER_NUMBER
+ORDER BY COUNT(*) DESC;
