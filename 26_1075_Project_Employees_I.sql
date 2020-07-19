97e12168420dbd004b7bf5984743fb479655f657
Saurabh Soni <saurabhsoni5893@gmail.com>
2020-07-19 10:48:04 -0500

    Create 26_1075_Project_Employees_I.sql

---
+++ 26_1075_Project_Employees_I.sql
@@ -0,0 +1,85 @@
+/*
+
+Table: Project
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| project_id  | int     |
+| employee_id | int     |
++-------------+---------+
+(project_id, employee_id) is the primary key of this table.
+employee_id is a foreign key to Employee table.
+
+Table: Employee
+
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| employee_id      | int     |
+| name             | varchar |
+| experience_years | int     |
++------------------+---------+
+employee_id is the primary key of this table.
+ 
+
+Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+
+The query result format is in the following example:
+
+Project table:
++-------------+-------------+
+| project_id  | employee_id |
++-------------+-------------+
+| 1           | 1           |
+| 1           | 2           |
+| 1           | 3           |
+| 2           | 1           |
+| 2           | 4           |
++-------------+-------------+
+
+Employee table:
++-------------+--------+------------------+
+| employee_id | name   | experience_years |
++-------------+--------+------------------+
+| 1           | Khaled | 3                |
+| 2           | Ali    | 2                |
+| 3           | John   | 1                |
+| 4           | Doe    | 2                |
++-------------+--------+------------------+
+
+Result table:
++-------------+---------------+
+| project_id  | average_years |
++-------------+---------------+
+| 1           | 2.00          |
+| 2           | 2.50          |
++-------------+---------------+
+The average experience years for the first project is (3 + 2 + 1) / 3 = 2.00 and for the second project is (3 + 2) / 2 = 2.50
+
+*/
+
+-- SQL Schema:
+
+Create table Project (project_id int, employee_id int)
+Create table Employee (employee_id int, name varchar(10), experience_years int)
+
+Truncate table Project
+insert into Project (project_id, employee_id) values ('1', '1')
+insert into Project (project_id, employee_id) values ('1', '2')
+insert into Project (project_id, employee_id) values ('1', '3')
+insert into Project (project_id, employee_id) values ('2', '1')
+insert into Project (project_id, employee_id) values ('2', '4')
+
+Truncate table Employee
+insert into Employee (employee_id, name, experience_years) values ('1', 'Khaled', '3')
+insert into Employee (employee_id, name, experience_years) values ('2', 'Ali', '2')
+insert into Employee (employee_id, name, experience_years) values ('3', 'John', '1')
+insert into Employee (employee_id, name, experience_years) values ('4', 'Doe', '2')
+
+-- Solution:
+
+SELECT project_id, round(cast(sum(experience_years) as decimal(3,2))/cast(count(p.employee_id) as decimal(3,2)), 2) as average_years
+FROM project p inner join employee e
+ON p.employee_id = e.employee_id
+GROUP BY project_id
