1fa78588755c73486fd7ecaf29831fa70823442a
Saurabh Soni <saurabhsoni5893@gmail.com>
2020-07-19 10:48:55 -0500

    Create 27_1076_Project_Employees_II.sql

---
+++ 27_1076_Project_Employees_II.sql
@@ -0,0 +1,101 @@
+/*
+
+Table: Project
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| project_id  | int     |
+| employee_id | int     |
++-------------+---------+
+(project_id, employee_id) is the primary key of this table.
+employee_id is a foreign key to Employee table.
+
+Table: Employee
+
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| employee_id      | int     |
+| name             | varchar |
+| experience_years | int     |
++------------------+---------+
+employee_id is the primary key of this table.
+ 
+
+Write an SQL query that reports all the projects that have the most employees.
+
+The query result format is in the following example:
+
+Project table:
++-------------+-------------+
+| project_id  | employee_id |
++-------------+-------------+
+| 1           | 1           |
+| 1           | 2           |
+| 1           | 3           |
+| 2           | 1           |
+| 2           | 4           |
++-------------+-------------+
+
+Employee table:
++-------------+--------+------------------+
+| employee_id | name   | experience_years |
++-------------+--------+------------------+
+| 1           | Khaled | 3                |
+| 2           | Ali    | 2                |
+| 3           | John   | 1                |
+| 4           | Doe    | 2                |
++-------------+--------+------------------+
+
+Result table:
++-------------+
+| project_id  |
++-------------+
+| 1           |
++-------------+
+The first project has 3 employees while the second one has 2.
+
+*/
+
+-- SQL Schema:
+
+Create table Project (project_id int, employee_id int)
+Create table Employee (employee_id int, name varchar(10), experience_years int)
+
+Truncate table Project
+insert into Project (project_id, employee_id) values ('1', '1')
+insert into Project (project_id, employee_id) values ('1', '2')
+insert into Project (project_id, employee_id) values ('1', '3')
+insert into Project (project_id, employee_id) values ('2', '1')
+insert into Project (project_id, employee_id) values ('2', '4')
+
+Truncate table Employee
+insert into Employee (employee_id, name, experience_years) values ('1', 'Khaled', '3')
+insert into Employee (employee_id, name, experience_years) values ('2', 'Ali', '2')
+insert into Employee (employee_id, name, experience_years) values ('3', 'John', '1')
+insert into Employee (employee_id, name, experience_years) values ('4', 'Doe', '2')
+
+-- Solution 1:
+
+SELECT project_id
+FROM(
+SELECT project_id, DENSE_RANK() OVER (ORDER BY COUNT(employee_id) DESC) AS RANK
+FROM project
+GROUP BY project_id) a
+WHERE a.rank = 1
+
+-- Solution 2:
+
+WITH CTE AS(
+SELECT project_id, COUNT(employee_id) AS cnt
+FROM project
+GROUP BY project_id)
+
+, CTE1 AS(
+SELECT project_id, MAX(cnt) OVER() as max
+FROM CTE)
+
+SELECT DISTINCT CTE.project_id
+FROM CTE JOIN CTE1 ON CTE.project_id = CTE1.project_id
+WHERE cnt = max
